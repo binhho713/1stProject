33315283bea29ba03f20d533ca4a2ddbea984dc1
binhho713 <139981030+binhho713@users.noreply.github.com>
2023-07-28 20:58:28 +0700

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -255,7 +255,7 @@ class ResNet_predictor(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc1 = nn.Linear(512 * block.expansion, num_classes)
         self.fc = nn.Linear(512 * block.expansion, emb_dim)
-        self.softmax = torch.nn.Softmax(dim=1)
+        self.sigmoid = torch.nn.Sigmoid()
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -311,7 +311,7 @@ class ResNet_predictor(nn.Module):
 
         x = self.avgpool(x)
         x = x.reshape(x.size(0), -1)
-        x1 = self.softmax(self.fc1(x))
+        x1 = self.sigmoid(self.fc1(x))
         x_emb = self.fc(x)
 
         if not emb:

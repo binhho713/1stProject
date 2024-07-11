b94b569664f471d632c283ea23fb60aca1c912c3
binhho713 <139981030+binhho713@users.noreply.github.com>
2024-07-11 16:57:54 +0700

    Add files via upload

---
+++ losses.py
@@ -0,0 +1,48 @@
+"""
+Functions to compute loss objectives of FedX.
+
+"""
+
+import torch
+
+from utils import F
+
+
+def nt_xent(x1, x2, t=0.1):
+    """Contrastive loss objective function"""
+    x1 = F.normalize(x1, dim=1)
+    x2 = F.normalize(x2, dim=1)
+    batch_size = x1.size(0)
+    out = torch.cat([x1, x2], dim=0)
+    sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / t)
+    mask = (torch.ones_like(sim_matrix) - torch.eye(2 * batch_size, device=sim_matrix.device)).bool()
+    sim_matrix = sim_matrix.masked_select(mask).view(2 * batch_size, -1)
+    pos_sim = torch.exp(torch.sum(x1 * x2, dim=-1) / t)
+    pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+    loss = (-torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+    return loss
+
+
+def js_loss(x1, x2, xa, t=0.1, t2=0.01):
+    """Relational loss objective function"""
+    pred_sim1 = torch.mm(F.normalize(x1, dim=1), F.normalize(xa, dim=1).t())
+    inputs1 = F.log_softmax(pred_sim1 / t, dim=1)
+    pred_sim2 = torch.mm(F.normalize(x2, dim=1), F.normalize(xa, dim=1).t())
+    inputs2 = F.log_softmax(pred_sim2 / t, dim=1)
+    target_js = (F.softmax(pred_sim1 / t2, dim=1) + F.softmax(pred_sim2 / t2, dim=1)) / 2
+    js_loss1 = F.kl_div(inputs1, target_js, reduction="batchmean")
+    js_loss2 = F.kl_div(inputs2, target_js, reduction="batchmean")
+    return (js_loss1 + js_loss2) / 2.0
+
+#ProtoFL loss
+
+def proto_dis_loss(v, r1 ,r2, balance_weight):
+
+    con_loss = 1 - F.cosine_similarity(r1, r2)
+    proto_loss = F.kl_div(v, r1, reduction="batchmean") + F.kl_div(v, r2 , reduction="batchmean")
+    loss = (1 - balance_weight) * proto_loss + balance_weight * con_loss
+    return loss
+
+def second_loss():
+
+    return loss
